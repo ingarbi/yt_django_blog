105885fdf12580956bb8b1528be3747d8d6cd4f1
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-03-14 22:04:58 +0300

    #8 styling

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
+
 
 class HomeView(ListView):
     model = Post
     template_name = "blog/index.html"
+    context_object_name = "posts"
